f1867f0d5a3c610935ae51b1ad8defb52c53a5e5
Tomas Dusik <tomas.dusik@mautilus.com>
2017-01-11 12:56:37 +0100

    sass

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { TodosComponent } from './todos/todos.component';
 
 const APP_ROUTES: Routes = [
+	{path: '', pathMatch: 'full', redirectTo: 'home'},
 	{path: 'home', component: HomeComponent},
 	{path: 'todos', component: TodosComponent},
 	{path: 'user', component: UserComponent}
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -4,7 +4,7 @@ import { TodoService } from '../todo.service';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
   todos; 
